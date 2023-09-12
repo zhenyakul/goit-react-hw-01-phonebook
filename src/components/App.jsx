32a68482d1ef035a0da86547decdc3776f91b69a
zhenyakul <zhekaboy04@gmail.com>
2023-09-12 08:55:20 +0200

    add Statistics component and css style for Profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,20 @@
 import { Profile } from "./Profile";
+import { Statistics } from "./Statistics";
 import user from '../user.json'
+import data from '../data.json'
 
 export const App = () => {
-  return (
+  return ([
   <Profile
   username={user.username}
   tag={user.tag}
   location={user.location}
   avatar={user.avatar}
   stats={user.stats}
-  />
+  /> ,
+  <Statistics
+  title="Upload stats"
+  stats={data}
+  />]
   );
 };
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,30 +2,30 @@ import style from './profile.css'
 
 export const Profile = ({username, tag, location, avatar, stats}) => {
     return(
-    <div class="profile">
-    <div class="description">
+    <div className={ style.profile }>
+    <div className="description">
       <img
         src={avatar}
         alt="User avatar"
-        class="avatar"
+        className="avatar"
       />
-      <p class="name">{username}</p>
-      <p class="tag">@{tag}</p>
-      <p class="location">{location}</p>
+      <p className="name">{username}</p>
+      <p className="tag">@{tag}</p>
+      <p className="location">{location}</p>
     </div>
   
-    <ul class="stats">
+    <ul className="stats">
       <li>
-        <span class="label">Followers</span>
-        <span class="quantity">{stats.followers}</span>
+        <span className="label">Followers</span>
+        <span className="quantity">{stats.followers}</span>
       </li>
       <li>
-        <span class="label">Views</span>
-        <span class="quantity">{stats.views}</span>
+        <span className="label">Views</span>
+        <span className="quantity">{stats.views}</span>
       </li>
       <li>
-        <span class="label">Likes</span>
-        <span class="quantity">{stats.likes}</span>
+        <span className="label">Likes</span>
+        <span className="quantity">{stats.likes}</span>
       </li>
     </ul>
   </div>
